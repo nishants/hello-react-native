415e0b2e2ec48d2cfbeb4e9a9e3b0bf52a0383f6
nishant <nishant.singh87@gmail.com>
2018-02-13 13:24:07 +0530

    Add a button and text field, create layout.

--- App.js
+++ App.js
@@ -10,11 +10,14 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <TextInput
-            placeholder={"Awesome Places"}
-            style={{width: 300}}
-            value={this.state.placeName}
-            onChangeText={this.updatePlace}/>
+        <View style={styles.rowContainer}>
+          <TextInput
+              placeholder={"An Awesome Place"}
+              style={styles.placeInput}
+              value={this.state.placeName}
+              onChangeText={this.updatePlace}/>
+          <Button onPress={()=> {}} style={styles.button} title="ADD"/>
+        </View>
       </View>
     );
   }
@@ -22,15 +25,26 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    //flex: 1,
-    padding        : 26,
-    flexDirection  : "column",
-    backgroundColor: '#fff',
+    flex: 1,
+    padding       : 26,
+    //flexDirection  : "column",
+    backgroundColor: '#eee',
     alignItems     : 'center',
     justifyContent : "flex-start",
   },
+  rowContainer: {
+    //flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  placeInput:{
+    width: "70%",
+    backgroundColor: '#fff',
+  },
   button: {
-    color: "black",
-    backgroundColor: '#eee',
+    color: "#fbfbfb",
+    width: "30%",
   }
+
 });
